65f279f18e2c9593ea6461ddec47f59993cd35c8
j.freese <freese.johannes@gmail.com>
2016-06-12 18:35:07 +0200

    partial commit

---
+++ app/src/main/java/de/roughriders/jf/eta/helpers/DistanceNotificationService.java
@@ -0,0 +1,19 @@
+package de.roughriders.jf.eta.helpers;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by b0wter on 6/12/16.
+ */
+public class DistanceNotificationService extends IntentService {
+
+    public DistanceNotificationService(){
+        super("DistanceNotificationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
