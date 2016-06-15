98341fdbc946742e6080aa26856df3bcfabd0955
j.freese <freese.johannes@gmail.com>
2016-06-15 22:53:20 +0200

    Changes to the service, registration of service in the manifest. Added missing clocklisteners ...

--- app/src/main/java/de/roughriders/jf/eta/adapters/RecentDestinationsAdapter.java
+++ app/src/main/java/de/roughriders/jf/eta/adapters/RecentDestinationsAdapter.java
@@ -13,14 +13,17 @@ import java.util.Collection;
 import java.util.List;
 
 import de.roughriders.jf.eta.R;
+import de.roughriders.jf.eta.helpers.IRecyclerIndexedViewItemClicked;
+import de.roughriders.jf.eta.helpers.IRecyclerViewItemClicked;
 import de.roughriders.jf.eta.models.RecentDestination;
 
 /**
  * Created by b0wter on 6/10/16.
  */
-public class RecentDestinationsAdapter extends RecyclerView.Adapter<RecentDestinationsAdapter.ViewHolder> {
+public class RecentDestinationsAdapter extends RecyclerView.Adapter<RecentDestinationsAdapter.ViewHolder> implements IRecyclerIndexedViewItemClicked {
 
     ArrayList<RecentDestination> recentDestinations;
+    List<IRecyclerViewItemClicked<RecentDestination>> itemClickedListeners = new ArrayList<>();
 
     public RecentDestinationsAdapter(Context context){
         recentDestinations = RecentDestination.getFromSharedPreferences(context);
@@ -47,7 +50,7 @@ public class RecentDestinationsAdapter extends RecyclerView.Adapter<RecentDestin
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_recentdestinations, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        ViewHolder holder = new ViewHolder(view, this);
         return holder;
     }
 
@@ -63,15 +66,38 @@ public class RecentDestinationsAdapter extends RecyclerView.Adapter<RecentDestin
         return recentDestinations.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public void addOnItemclickedListener(IRecyclerViewItemClicked<RecentDestination> listener){
+        itemClickedListeners.add(listener);
+    }
+
+    public void removeOnItemClickedListener(IRecyclerViewItemClicked<RecentDestination> listener){
+        itemClickedListeners.remove(listener);
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        for(IRecyclerViewItemClicked<RecentDestination> listener : itemClickedListeners)
+            listener.onItemclicked(recentDestinations.get(position));
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public TextView primaryTextView;
         public TextView secondaryTextView;
+        private IRecyclerIndexedViewItemClicked itemClickedListener;
 
-        public ViewHolder(View view){
+        public ViewHolder(View view, IRecyclerIndexedViewItemClicked onItemClickedListener){
             super(view);
+            view.setOnClickListener(this);
             primaryTextView = (TextView)view.findViewById(R.id.viewholder_recentdestinations_primarytext);
             secondaryTextView = (TextView)view.findViewById(R.id.viewholder_recentdestinations_secondarytext);
+            itemClickedListener = onItemClickedListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(itemClickedListener != null)
+                itemClickedListener.onItemClicked(getAdapterPosition());
         }
     }
 }
