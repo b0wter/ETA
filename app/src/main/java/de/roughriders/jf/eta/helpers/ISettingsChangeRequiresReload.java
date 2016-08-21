d9fd74b76f3149380af7eba19900350150a1b916
b0wter <evil-klabautermann@gmx.de>
2016-08-21 12:46:11 +0200

    Added the missing functionality of updating the recycler views in the MainActivity after clearing them in the SettingsActivity.

--- app/src/main/java/de/roughriders/jf/eta/helpers/ISettingsChangeRequiresReload.java
+++ app/src/main/java/de/roughriders/jf/eta/helpers/ISettingsChangeRequiresReload.java
@@ -4,5 +4,5 @@ package de.roughriders.jf.eta.helpers;
  * Created by b0wter on 21-Aug-16.
  */
 public interface ISettingsChangeRequiresReload {
-    void onRequiresReload(String entityName);
+    void onRequiresReload(String entityName, boolean clearedAll);
 }
