522e167444b3c76beacf8a2328b239da3f0d9a59
b0wter <evil-klabautermann@gmx.de>
2016-10-14 16:28:40 +0200

    some cleanup, removed unnecessary files

--- app/src/main/java/de/roughriders/jf/eta/helpers/ApiHelper.java
+++
@@ -1,8 +0,0 @@
-package de.roughriders.jf.eta.helpers;
-
-/**
- * Created by b0wter on 6/9/16.
- * Responsible for the interaction with the Google API (Places, FusedLocationService, Distance Matrix,...).
- */
-public class ApiHelper {
-}
